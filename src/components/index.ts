769f0cd6dff7491269b90da78f94c8fa54602930
Chopperqt <ilua292010@gmail.com>
2024-02-07 22:32:54 +0600

    feat: добавление компонента Тэг
    n

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button";
 export { default as Icon } from "./Icon";
 export { default as Loader } from "./Loader";
-export { default as Input } from './Input';
-export { default as Checkbox } from './Checkbox'
+export { default as Input } from "./Input";
+export { default as Checkbox } from "./Checkbox";
+export { default as Tag } from "./Tag";
