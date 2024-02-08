28ff9299a2790cb07dc23a162f6ba840378fb926
Chopperqt <ilua292010@gmail.com>
2024-02-08 23:15:59 +0600

    feat: добавление стилей для компонента Tag
    Были добавлены цвета и пропсы для компонента Tag

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import cx from 'classnames'
 
-const Tag = () => {
-  return <div>asdfjsdf</div>;
+import s from "./Tag.module.scss";
+
+export interface TagProps {
+  text?: string;
+  isInteractive?: boolean
+}
+const Tag = ({
+  text = '',
+  isInteractive = false,
+}: TagProps) => {
+  return (
+    <div
+      className={cx(s.layout, {
+        [s.layoutInteracitve]: isInteractive,
+      })}
+    >
+      <span>{text}</span>
+    </div>
+  );
 };
 
 export default Tag;
